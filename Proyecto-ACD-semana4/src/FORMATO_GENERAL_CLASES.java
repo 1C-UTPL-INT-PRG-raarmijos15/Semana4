5521ed093ae4ad3a180e5af01a649b460a15d8f6
Raul-Armijos <raarmijos15@utpl.edu.ec>
2025-10-29 12:08:04 -0500

    semana 4

---
+++ Proyecto-ACD-semana4/src/FORMATO_GENERAL_CLASES.java
@@ -0,0 +1,16 @@
+//[nivelAcceso-de-la clase: public, private, protected] [palabra clave para definir la clase: class]
+//[Nombre-de-la-clase: NombreClase]
+public class FORMATO_GENERAL_CLASES{//REPRESENTA EL INICIO DEL ALGORITMO
+        //CODIFICAR TODOS LOS METODOS DESEADOS
+    
+        //[nivel de acceso del metodo: public,private, protected.]
+        //{[alcance-memoria-del-metodo: static]} "OPCIONAL"
+        //[Tipo-de-retorno/respuesta-del-metodo: Void, int, double, boolean, ...etc]
+        //[NOMBRE-DEL-METODO: main (como metodo inicial)]
+        public static void main(String[] args)
+        { // Representa inicio del metodo main
+          // TODAS LAS SENTENCIAS O CODIGO
+        } // Representa fin del metodo main
+        
+}//REPRESENTA EL FIN DEL ALGORITMO
+                                            
